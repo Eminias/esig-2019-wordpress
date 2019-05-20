cb9e8bde5e03b211067299608da606cf26c7fb93
anja.scheidegger <anja.scheidegger@divcom.ch>
2019-05-20 10:57:28 +0200

    Ajouter commentaire ajouter dossier ico_plugin-exemple

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -7,3 +7,111 @@ Plugin Name: Mon premier plugin
 function mon_plugin_meta_keywords() {
     echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
 }
+/*//Ajout d'une action sur 'wp_head' qui appellera mon_plugin_meta_keywords()
+//add_action('wp_head', 'mon_plugin_meta_keywords');
+
+//Fonction qui envoie par email les infos d'un email supprimé
+function mon_plugin_post_delete_mail($post_id) {
+    //Récupére les informations de l'article supprimé
+    $post = get_post($post_id);
+    //Création du sujet de l'email
+    $sujet = "Artile supprimé :" . $post->post_title;
+    //Création du contenu de l'email
+    $message = "Contenu de l'artilce : " . $post->post_content;
+    //Envoi de l'email à l'administrateur du site
+    wp_mail(get_bloginfo('admin_email'), $sujet, $message);
+}
+//Ajout d'une action sur 'delete_post' qui appellera mon_plugin_post_delete_mail()
+add_action('delete_post', 'mon_plugin_post_delete_mail');
+
+//Fonction qui remplace la chaine 'et' par '&amp;'
+function mon_plugin_the_title( $title ) {
+    //Remplace 'et' dans le titre
+    $title = str_replace( 'et', '&amp;', $title );
+    //Retourne le titre modifié
+    return $title;
+}
+//Ajout d'un filtre sur 'the_title' qui appellera mon_plugin_the_title()
+add_filter( 'the_title', 'mon_plugin_the_title' );
+
+//Fonction qui retourne la célèbre citation de maître Yoda
+function mon_plugin_yoda_shortcode() {
+    return "<blockquote>Que la force soit avec toi jeune padawan !</blockquote>";
+}
+//Enregistre le shortcode yoda
+add_shortcode( 'yoda', 'mon_plugin_yoda_shortcode' );
+
+//Fonction de rappel qui retourne la célèbre citation de maître Yoda
+function mon_plugin_yoda_shortcode() {
+    return "<blockquote>Que la force soit avec toi jeune padawan !</blockquote>";
+}
+
+//Enregistre les shortcodes du plugin
+function mon_plugin_register_shortcode() {
+    add_shortcode( 'yoda', 'mon_plugin_yoda_shortcode' );
+}
+add_action( 'init', 'mon_plugin_register_shortcode' );
+
+/**
+ * Shortcode qui retourne le célèbre "Luke, Je sui ton père !" dans un élément blockquote.
+ * Le contenu du shortcode sera utilisé pour remplacer 'Luke'
+ *
+ * Exemples :
+ * [vador] => <blockquote>Luke, Je sui ton père !</blockquote>
+ * [vador]Serge[/vador] => <blockquote>Serge, Je sui ton père !</blockquote>
+ */
+/*
+function mon_plugin_vador_shortcode($atts, $content = "") {
+    //Si contenu vide
+    if (empty( $content )) {
+        $content = 'Luke';
+    }
+    return "<blockquote>" . $content . ", Je suis ton père !</blockquote>";
+}
+
+//Fonction de rappel qui retourne la célèbre citation de maître Yoda
+function mon_plugin_yoda_shortcode() {
+    return "<blockquote>Que la force soit avec toi jeune padawan !</blockquote>";
+}
+
+//Enregistre les shortcodes du plugin
+function mon_plugin_register_shortcode() {
+    add_shortcode( 'yoda', 'mon_plugin_yoda_shortcode' );
+    add_shortcode( 'vador', 'mon_plugin_vador_shortcode' );
+}
+add_action( 'init', 'mon_plugin_register_shortcode' );
+
+/**
+ * Shortcode qui retourne le célèbre "Luke, Je sui ton père !" dans un élément blockquote.
+ * Le contenu du shortcode sera utilisé pour remplacer 'Luke'
+ * L'attribut tag du shortcode permet de remplacer l'élément blockquote par : p, h1, h2 ou div
+ *
+ * Exemples :
+ * [vador] => <blockquote>Luke, Je sui ton père !</blockquote>
+ * [vador]Serge[/vador] => <blockquote>Serge, Je sui ton père !</blockquote>
+ * [vador tag="div"]Jean-Marc[/vador] => <div>Jean-Marc, Je sui ton père !</div>
+ */
+/*
+function mon_plugin_vador_shortcode($atts, $content = "") {
+    // Tag par défaut
+    $tag = 'blockquote';
+
+    // Si $tag valide on le récupère
+    if(isset($atts['tag']) AND in_array($atts['tag'], ['p','h1','h2','div'])) {
+        $tag = $atts['tag'];
+    }
+
+    // Si contenu vide
+    if (empty( $content )) {
+        $content = 'Luke';
+    }
+
+    return '<' . $tag . '>' . $content . ', Je suis ton père !' . '';
+}
+echo do_shortcode('Exemple de shortcode : [yoda]');
+//Exemple de shotcode <blockquote>Que la force soit avec toi jeune padawan !</blockquote>
+
+//Fonction qui retourne le résultat d'un shortcode dans un élément strong
+function mon_plugin_strong_shortcode() {
+    return '<strong>' . do_shortcode( $content ) . '</strong>';
+}*/
