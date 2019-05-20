c40858105a3319feebedae838d8903c6b33503fe
anja.scheidegger <anja.scheidegger@divcom.ch>
2019-05-20 09:31:08 +0200

    Ajouter une fonction meta keywords

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -2,3 +2,8 @@
 /*
 Plugin Name: Mon premier plugin
  */
+
+//Fonction qui affiche la balise meta
+function mon_plugin_meta_keywords() {
+    echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
+}
